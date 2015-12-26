8a623abf6565f0b364aef21d47af78a7ccf4855b
James <CrazyPython@users.noreply.github.com>
2015-12-26 14:47:17 -0500

    Update interpreter.py

--- interpreter.py
+++ interpreter.py
@@ -1,7 +1,10 @@
 import requests, sys
 try:
-    f = open(sys.argv[2])
+    f = open(sys.argv[1])
 except IOError:
     sys.stderr.write("Could not find file for reading.")
+    sys.exit()
+except KeyError:
+    sys.stderr.write("No file given.")
     sys.exit()
 print(requests.get("http://bit.ly/"+f.read()))
